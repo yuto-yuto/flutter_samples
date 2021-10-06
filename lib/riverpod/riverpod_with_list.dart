660f6e53e10990dfd5827e333f16746118aa914d
yuto-yuto <yuto.nakatsu@gmail.com>
2021-10-06 21:02:21 +0200

    add riverpod with list sample

--- lib/riverpod/riverpod_with_list.dart
+++ lib/riverpod/riverpod_with_list.dart
@@ -9,6 +9,7 @@ class RiverpodWithList extends StatelessWidget {
       child: PageView(
         children: [
           _View1(),
+          _View2(),
         ],
       ),
     );
@@ -17,7 +18,7 @@ class RiverpodWithList extends StatelessWidget {
 
 class _View1 extends ConsumerWidget {
   final List<String> _texts = [];
-  late final StateProvider<int> _lengthProvider = StateProvider((ref) => 0);
+  final StateProvider<int> _lengthProvider = StateProvider((ref) => 0);
 
   @override
   Widget build(BuildContext context, ScopedReader watch) {
@@ -32,7 +33,9 @@ class _View1 extends ConsumerWidget {
     final listView = ListView.builder(
       itemCount: watch(_lengthProvider).state,
       itemBuilder: (context, index) {
-        return Card(child: Text(_texts[index] + "/${_texts.length}"));
+        return ListTile(
+          title: Text(_texts[index] + "/${_texts.length}"),
+        );
       },
     );
 
@@ -46,33 +49,41 @@ class _View1 extends ConsumerWidget {
   }
 }
 
-// class _View2 extends ConsumerWidget {
-//   final List<String> _texts = [];
-//   late final StateProvider<int> _lengthProvider = StateProvider((ref) => 0);
+class _View2 extends ConsumerWidget {
+  final List<String> _texts = [];
+  final StateProvider<int> _lengthProvider = StateProvider((ref) => 0);
+  final List<StateProvider<int>> _countProviderList = [];
 
-//   @override
-//   Widget build(BuildContext context, ScopedReader watch) {
-//     final button = IconButton(
-//       onPressed: () {
-//         _texts.add("string-${_texts.length + 1}");
-//         context.read(_lengthProvider).state = _texts.length;
-//       },
-//       icon: Icon(Icons.add),
-//     );
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final button = IconButton(
+      onPressed: () {
+        _texts.add("string-${_texts.length + 1}");
+        _countProviderList.add(StateProvider((ref) => 0));
+        context.read(_lengthProvider).state = _texts.length;
+      },
+      icon: Icon(Icons.add),
+    );
 
-//     final listView = ListView.builder(
-//       itemCount: watch(_lengthProvider).state,
-//       itemBuilder: (context, index) {
-//         return Card(child: Text(_texts[index]));
-//       },
-//     );
+    final listView = ListView.builder(
+      itemCount: watch(_lengthProvider).state,
+      itemBuilder: (context, index) {
+        final text = _texts[index] +
+            "/${_texts.length}" +
+            "\t${watch(_countProviderList[index]).state.toString()}";
+        return ListTile(
+          title: Text(text),
+          onTap: () => context.read(_countProviderList[index]).state++,
+        );
+      },
+    );
 
-//     return Scaffold(
-//       appBar: AppBar(title: Text("List wth StateProvider")),
-//       body: Column(children: [
-//         Center(child: button),
-//         Expanded(child: listView),
-//       ]),
-//     );
-//   }
-// }
+    return Scaffold(
+      appBar: AppBar(title: Text("List with StateProvider2")),
+      body: Column(children: [
+        Center(child: button),
+        Expanded(child: listView),
+      ]),
+    );
+  }
+}
