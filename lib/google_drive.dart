b9f0e0366fa49e759d94461c4b784d5b1e13682e
yuto-yuto <yuto.nakatsu@gmail.com>
2021-12-15 10:23:12 +0100

    upload data to google drive

--- lib/google_drive.dart
+++ lib/google_drive.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:googleapis/drive/v3.dart';
+import 'package:flutter_samples/show_dialog.dart';
+import 'package:googleapis/drive/v3.dart' as drive;
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 class GoogleDriveTest extends StatefulWidget {
   @override
@@ -11,8 +14,16 @@ class GoogleDriveTest extends StatefulWidget {
 
 class _GoogleDriveTest extends State<GoogleDriveTest> {
   bool _loginStatus = false;
-  final googleSignIn =
-      GoogleSignIn.standard(scopes: [DriveApi.driveAppdataScope]);
+  final googleSignIn = GoogleSignIn.standard(scopes: [
+    drive.DriveApi.driveAppdataScope,
+    drive.DriveApi.driveFileScope,
+  ]);
+
+  @override
+  void initState() {
+    _loginStatus = googleSignIn.currentUser != null;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,12 +50,34 @@ class _GoogleDriveTest extends State<GoogleDriveTest> {
       },
       child: Text("Sing out"),
     );
+    final uploadToHidden = ElevatedButton(
+      onPressed: () {
+        _uploadToHidden();
+      },
+      child: Text("Upload to app folder (hidden)"),
+    );
+    final uploadToNormal = ElevatedButton(
+      onPressed: () {
+        _uploadToNormal();
+      },
+      child: Text("Upload to normal folder"),
+    );
+    final showList = ElevatedButton(
+      onPressed: () {
+        _showList();
+      },
+      child: Text("Show the data list"),
+    );
 
     return Column(
       children: [
         Center(child: Text("Sign in status: ${_loginStatus ? "In" : "Out"}")),
         Center(child: signIn),
         Center(child: signOut),
+        Divider(),
+        Center(child: uploadToHidden),
+        Center(child: uploadToNormal),
+        Center(child: showList),
       ],
     );
   }
@@ -64,7 +97,9 @@ class _GoogleDriveTest extends State<GoogleDriveTest> {
 
         assert(loginUser.user?.uid == FirebaseAuth.instance.currentUser?.uid);
         print("Sign in");
-        _loginStatus = true;
+        setState(() {
+          _loginStatus = true;
+        });
       }
     } catch (e) {
       print(e);
@@ -74,7 +109,189 @@ class _GoogleDriveTest extends State<GoogleDriveTest> {
   Future<void> _signOut() async {
     await FirebaseAuth.instance.signOut();
     await googleSignIn.signOut();
-    _loginStatus = false;
+    setState(() {
+      _loginStatus = false;
+    });
     print("Sign out");
+  }
+
+  Future<void> _uploadToHidden() async {
+    final googleUser = await googleSignIn.signIn();
+    final headers = await googleUser?.authHeaders;
+    if (headers == null) {
+      await showMessage(context, "Sign-in first", "Error");
+      return;
+    }
+
+    // Not allow a user to do something else
+    showGeneralDialog(
+      context: context,
+      barrierDismissible: false,
+      transitionDuration: Duration(seconds: 2),
+      barrierColor: Colors.black.withOpacity(0.5),
+      pageBuilder: (context, animation, secondaryAnimation) => Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+
+    final client = GoogleAuthClient(headers);
+    final driveApi = drive.DriveApi(client);
+
+    // Create data here instead of loading a file
+    final contents = "Technical Feeder";
+    final Stream<List<int>> mediaStream =
+        Future.value(contents.codeUnits).asStream().asBroadcastStream();
+    var media = new drive.Media(mediaStream, contents.length);
+
+    // Set up File info
+    var driveFile = new drive.File();
+    final timestamp = DateFormat("yyyy-MM-dd-hhmmss").format(DateTime.now());
+    driveFile.name = "technical-feeder-$timestamp.txt";
+    driveFile.modifiedTime = DateTime.now().toUtc();
+    driveFile.parents = ["appDataFolder"];
+
+    // Upload
+    final response = await driveApi.files.create(driveFile, uploadMedia: media);
+    print("response: $response");
+
+    // simulate a slow process
+    await Future.delayed(Duration(seconds: 2));
+    // Remove a dialog
+    Navigator.pop(context);
+  }
+
+  Future<String?> _getFolderId(drive.DriveApi driveApi) async {
+    final mimeType = "application/vnd.google-apps.folder";
+    String folderName = "Flutter-sample-by-tf";
+
+    try {
+      final found = await driveApi.files.list(
+        q: "mimeType = '$mimeType' and " + "name = '$folderName'",
+        $fields: "files(id, name)",
+      );
+      final files = found.files;
+      if (files == null) {
+        await showMessage(context, "Sign-in first", "Error");
+        return null;
+      }
+
+      if (files.isNotEmpty) {
+        return files.first.id;
+      }
+
+      // Create a folder
+      var folder = new drive.File();
+      folder.name = folderName;
+      folder.mimeType = mimeType;
+      final folderCreation = await driveApi.files.create(folder);
+      print("Folder ID: ${folderCreation.id}");
+
+      return folderCreation.id;
+    } catch (e) {
+      print(e);
+      // I/flutter ( 6132): DetailedApiRequestError(status: 403, message: The granted scopes do not give access to all of the requested spaces.)
+      return null;
+    }
+  }
+
+  Future<void> _uploadToNormal() async {
+    final googleUser = await googleSignIn.signIn();
+    final headers = await googleUser?.authHeaders;
+    if (headers == null) {
+      await showMessage(context, "Sign-in first", "Error");
+      return;
+    }
+
+    try {
+      // Not allow a user to do something else
+      showGeneralDialog(
+        context: context,
+        barrierDismissible: false,
+        transitionDuration: Duration(seconds: 2),
+        barrierColor: Colors.black.withOpacity(0.5),
+        pageBuilder: (context, animation, secondaryAnimation) => Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+
+      final client = GoogleAuthClient(headers);
+      final driveApi = drive.DriveApi(client);
+
+      final folderId = await _getFolderId(driveApi);
+      if (folderId == null) {
+        await showMessage(context, "Failure", "Error");
+        return;
+      }
+
+      // Create data here instead of loading a file
+      final contents = "Technical Feeder";
+      final Stream<List<int>> mediaStream =
+          Future.value(contents.codeUnits).asStream().asBroadcastStream();
+      var media = new drive.Media(mediaStream, contents.length);
+
+      // Set up File info
+      var driveFile = new drive.File();
+      final timestamp = DateFormat("yyyy-MM-dd-hhmmss").format(DateTime.now());
+      driveFile.name = "technical-feeder-$timestamp.txt";
+      driveFile.modifiedTime = DateTime.now().toUtc();
+      driveFile.parents = [folderId];
+
+      // Upload
+      final response =
+          await driveApi.files.create(driveFile, uploadMedia: media);
+      print("response: $response");
+
+      // simulate a slow process
+      await Future.delayed(Duration(seconds: 2));
+    } finally {
+      // Remove a dialog
+      Navigator.pop(context);
+    }
+  }
+
+  Future<void> _showList() async {
+    final googleUser = await googleSignIn.signIn();
+    final headers = await googleUser?.authHeaders;
+    if (headers == null) {
+      await showMessage(context, "Sign-in first", "Error");
+      return;
+    }
+
+    final client = GoogleAuthClient(headers);
+    final driveApi = drive.DriveApi(client);
+
+    final fileList = await driveApi.files.list(
+        spaces: 'appDataFolder', $fields: 'files(id, name, modifiedTime)');
+    final files = fileList.files;
+    if (files == null) {
+      return showMessage(context, "Data not found", "");
+    }
+
+    final alert = AlertDialog(
+      title: Text("Item List"),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: files.map((e) => Text(e.name ?? "no-name")).toList(),
+        ),
+      ),
+    );
+
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => alert,
+    );
+  }
+}
+
+class GoogleAuthClient extends http.BaseClient {
+  final Map<String, String> _headers;
+  final _client = new http.Client();
+
+  GoogleAuthClient(this._headers);
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers.addAll(_headers);
+    return _client.send(request);
   }
 }
