a10f606238872222466765e06cd2dffa2ced3f75
yuto-yuto <yuto.nakatsu@gmail.com>
2021-12-17 10:05:22 +0100

    update tabbar

--- lib/tabbar.dart
+++ lib/tabbar.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyTabbar extends StatelessWidget {
+class MyTabbar extends StatefulWidget {
+  @override
+  _MyTabbar createState() => _MyTabbar();
+}
+
+class _MyTabbar extends State<MyTabbar> with SingleTickerProviderStateMixin {
+  late TabController _controller;
+  @override
+  void initState() {
+    _controller = TabController(length: 3, vsync: this, initialIndex: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final views = List.generate(
@@ -13,19 +31,72 @@ class MyTabbar extends StatelessWidget {
         body: Center(
           child: Text(
             "${index + 1}",
-            style: TextStyle(fontSize: 100),
+            style: TextStyle(fontSize: 50),
           ),
         ),
       ),
     );
 
     return SafeArea(
-      child: DefaultTabController(
-        length: views.length,
-        // child: _child1(views),
-        // child: _child2(views),
-        // child: _child3(context, views),
-        child: _child4(context, views),
+        child: SingleChildScrollView(
+      child: Column(
+        children: [
+          _wrap(
+            context,
+            DefaultTabController(length: views.length, child: _child1(views)),
+          ),
+          _wrap(
+            context,
+            DefaultTabController(length: views.length, child: _child2(views)),
+          ),
+          _wrap(
+            context,
+            DefaultTabController(
+              length: views.length,
+              child: _child3(context, views, _tabBar1()),
+            ),
+          ),
+          _wrap(
+            context,
+            DefaultTabController(
+              length: views.length,
+              child: _child3(context, views, _tabBar2()),
+            ),
+          ),
+          _wrap(
+            context,
+            DefaultTabController(
+              length: views.length,
+              child: _child3(context, views, _tabBar3()),
+            ),
+          ),
+          _wrap(
+            context,
+            DefaultTabController(
+              length: views.length,
+              child: _child4(context, views, _tabBar3()),
+            ),
+          ),
+          _wrap(
+            context,
+            _child5(context, views),
+          ),
+        ],
+      ),
+    ));
+  }
+
+  Widget _wrap(BuildContext context, Widget widget) {
+    return Container(
+      child: widget,
+      padding: const EdgeInsets.all(5),
+      margin: EdgeInsets.all(5),
+      height: MediaQuery.of(context).size.height * 0.3,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        border: Border.all(
+          color: Colors.red,
+        ),
       ),
     );
   }
@@ -53,9 +124,7 @@ class MyTabbar extends StatelessWidget {
     );
   }
 
-  Widget _child3(BuildContext context, List<Widget> views) {
-    // final tabBar = _tabBar1();
-    final tabBar = _tabBar2();
+  Widget _child3(BuildContext context, List<Widget> views, TabBar tabBar) {
     return Scaffold(
       body: TabBarView(
         children: views,
@@ -71,8 +140,7 @@ class MyTabbar extends StatelessWidget {
     );
   }
 
-  Widget _child4(BuildContext context, List<Widget> views) {
-    final tabBar = _tabBar5();
+  Widget _child4(BuildContext context, List<Widget> views, TabBar tabBar) {
     return Scaffold(
       body: TabBarView(
         children: views,
@@ -80,6 +148,19 @@ class MyTabbar extends StatelessWidget {
       appBar: PreferredSize(
         preferredSize: tabBar.preferredSize,
         child: tabBar,
+      ),
+    );
+  }
+
+  Widget _child5(BuildContext context, List<Widget> views) {
+    return Scaffold(
+      body: TabBarView(
+        controller: _controller,
+        children: views,
+      ),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(30),
+        child: _tabPageSelectogSelector(),
       ),
     );
   }
@@ -127,59 +208,60 @@ class MyTabbar extends StatelessWidget {
     );
   }
 
-  TabBar _tabBar5(BuildContext context) {
-    return TabBar(
-      tabs:[
-        Tab(),
-        Tab(),
-        Tab(),
-      ],
+  Widget _tabPageSelectogSelector() {
+    return Center(
+      child: TabPageSelector(
+        controller: _controller,
+        color: Colors.grey,
+        selectedColor: Colors.purple,
+        indicatorSize: 20,
+      ),
     );
   }
 
-  TabBar _tabBar4(BuildContext context) {
-    // return TabBar(
-    //   tabs: [
-    //     for (int i = 0; i < 3; i++)
-    //       Container(
-    //         child: SizedBox.shrink(),
-    //         // padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-    //         height: 10,
-    //         width: 50,
-    //         decoration: BoxDecoration(
-    //           borderRadius: BorderRadius.circular(10),
-    //           color: Colors.grey,
-    //         ),
-    //       )
-    //   ],
-    //   // indicator: BoxDecoration(
-    //   //   borderRadius: BorderRadius.circular(10),
-    //   //   backgroundBlendMode: BlendMode.clear,
-    //   //   color: Colors.black,
-    //   // ),
-    // );
-    final controller = DefaultTabController.of(context);
-    if (controller == null) {
-      throw Exception("no default tab controller");
-    }
+  // TabBar _tabBar4(BuildContext context) {
+  //   // return TabBar(
+  //   //   tabs: [
+  //   //     for (int i = 0; i < 3; i++)
+  //   //       Container(
+  //   //         child: SizedBox.shrink(),
+  //   //         // padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+  //   //         height: 10,
+  //   //         width: 50,
+  //   //         decoration: BoxDecoration(
+  //   //           borderRadius: BorderRadius.circular(10),
+  //   //           color: Colors.grey,
+  //   //         ),
+  //   //       )
+  //   //   ],
+  //   //   // indicator: BoxDecoration(
+  //   //   //   borderRadius: BorderRadius.circular(10),
+  //   //   //   backgroundBlendMode: BlendMode.clear,
+  //   //   //   color: Colors.black,
+  //   //   // ),
+  //   // );
+  //   final controller = DefaultTabController.of(context);
+  //   if (controller == null) {
+  //     throw Exception("no default tab controller");
+  //   }
 
-    return TabBar(
-      tabs: [
-        for (int i = 0; i < 3; i++)
-          AnimatedBuilder(
-              animation: controller.animation!,
-              builder: (context, widget) {
-                return Container(
-                  child: Text("View ${i + 1}"),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: controller.index == i ? Colors.black : Colors.grey,
-                  ),
-                );
-              },)
-      ],
-    );
-  }
+  //   return TabBar(
+  //     tabs: [
+  //       for (int i = 0; i < 3; i++)
+  //         AnimatedBuilder(
+  //           animation: controller.animation!,
+  //           builder: (context, widget) {
+  //             return Container(
+  //               child: Text("View ${i + 1}"),
+  //               padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+  //               decoration: BoxDecoration(
+  //                 borderRadius: BorderRadius.circular(12),
+  //                 color: controller.index == i ? Colors.black : Colors.grey,
+  //               ),
+  //             );
+  //           },
+  //         )
+  //     ],
+  //   );
+  // }
 }
