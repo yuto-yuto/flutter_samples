53365f62759ff8cc0afa89ca0fd3215c36c90d2c
yuto-yuto <yuto.nakatsu@gmail.com>
2023-01-13 06:47:06 +0100

    Update cross axis scroll

--- lib/cross_axis_scroll.dart
+++ lib/cross_axis_scroll.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class CrossAxisScroll extends StatelessWidget {
@@ -35,6 +37,10 @@ class CrossAxisScroll extends StatelessWidget {
               Divider(),
               Text("DataTable cross axis"),
               _wrap(context, _createDataTable()),
+              Text("DataTable cross axis scroll bar"),
+              _wrap(context, _createDataTableCrossAxisWithBar1(), height: 300),
+              Text("DataTable cross axis scroll bar 2"),
+              _wrap(context, _createDataTableCrossAxisWithBar2(), height: 300),
             ],
           ),
         ),
@@ -42,10 +48,10 @@ class CrossAxisScroll extends StatelessWidget {
     );
   }
 
-  Widget _wrap(BuildContext context, Widget widget) {
+  Widget _wrap(BuildContext context, Widget widget, {double? height}) {
     return Container(
       child: widget,
-      height: 150,
+      height: height ?? 150,
       width: MediaQuery.of(context).size.width,
       padding: EdgeInsets.all(5),
       margin: EdgeInsets.all(5),
@@ -114,6 +120,87 @@ class CrossAxisScroll extends StatelessWidget {
       child: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: dataTable,
+      ),
+    );
+  }
+
+  Widget _generateColorfulDataTable() {
+    final count = 20;
+    final colors = [Colors.black, Colors.red, Colors.yellow, Colors.blue];
+
+    return DataTable(
+      columns: List.generate(
+        count,
+        (index) => DataColumn(
+          label: Text("Column $index"),
+        ),
+      ),
+      rows: List.generate(
+        count,
+        (index) => DataRow(
+          cells: List.generate(
+            count,
+            (index2) => DataCell(
+              Container(
+                child: SizedBox(
+                  width: 50,
+                  height: 50,
+                ),
+                decoration: BoxDecoration(
+                    color: colors[(index + index2) % colors.length]),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  final _verticalScrollController1 = ScrollController();
+  final _horizontalScrollController1 = ScrollController();
+  Widget _createDataTableCrossAxisWithBar1() {
+    return Scrollbar(
+      controller: _verticalScrollController1,
+      thumbVisibility: true,
+      trackVisibility: true,
+      child: SingleChildScrollView(
+        controller: _verticalScrollController1,
+        scrollDirection: Axis.vertical,
+        child: Scrollbar(
+          controller: _horizontalScrollController1,
+          thumbVisibility: true,
+          trackVisibility: true,
+          child: SingleChildScrollView(
+            controller: _horizontalScrollController1,
+            scrollDirection: Axis.horizontal,
+            child: _generateColorfulDataTable(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  final _verticalScrollController2 = ScrollController();
+  final _horizontalScrollController2 = ScrollController();
+  Widget _createDataTableCrossAxisWithBar2() {
+    return Scrollbar(
+      controller: _verticalScrollController2,
+      thumbVisibility: true,
+      trackVisibility: true, // make the scrollbar easy to see
+      child: Scrollbar(
+        controller: _horizontalScrollController2,
+        thumbVisibility: true,
+        trackVisibility: true, // make the scrollbar easy to see
+        notificationPredicate: (notif) => notif.depth == 1,
+        child: SingleChildScrollView(
+          controller: _verticalScrollController2,
+          scrollDirection: Axis.vertical,
+          child: SingleChildScrollView(
+            controller: _horizontalScrollController2,
+            scrollDirection: Axis.horizontal,
+            child: _generateColorfulDataTable(),
+          ),
+        ),
       ),
     );
   }
